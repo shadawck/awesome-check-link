70b281e81fd7b5492f12fefb031eec422b9ba65d
HUGUET <hug211mire@gmail.com>
2020-07-24 21:03:11 +0200

    check_redirection

--- aclinks/checker.py
+++ aclinks/checker.py
@@ -43,13 +43,20 @@ def get_down_links(file, links, __verbose, __exit):
     
     for l in links :
         try:
-            r = requests.head(l[1][2],allow_redirects=True)
+            r = requests.head(l[1][2],allow_redirects=False)
+
             if r.status_code != 200:
+                
+                # Check for redirected url for the user to change to redirected url in md file
+                if check_history(r,__verbose)[0]:
+                    __verbose and print("At line", l[0] , ":" , l[1][2], ":", "Url is redirected to:", check_history(r,__verbose)[1])
+
+
                 # If test_https return true, we alert the user that the link is not down and need an update, else add to "down" list 
                 if r.status_code == 301 and test_https(l[1][2], __verbose):
                     __verbose and print("At line", l[0] , ":" , l[1][2], ":", "The site just switched for HTTPS.")
                     continue # Do not add the link to down list
-
+                
                 down.append(
                     (
                         l[0],
@@ -72,30 +79,14 @@ def get_down_links(file, links, __verbose, __exit):
 
     return down
 
-# For next version
-def remove_down_links(file, line_numbers):
-    """In a file, delete the lines at line number in given list"""
-    is_skipped = False
-    counter = 0
-    # Create name of dummy / temporary file
-    dummy_file = file + '.bak'
-    # Open original file in read only mode and dummy file in write mode
-    with open(file, 'r') as read_obj, open(dummy_file, 'w') as write_obj:
-        # Line by line copy data from original file to dummy file
-        for line in read_obj:
-            # If current line number exist in list then skip copying that line
-            if counter not in line_numbers:
-                write_obj.write(line)
-            else:
-                is_skipped = True
-            counter += 1
- 
-    # If any line is skipped then rename dummy file as original file
-    if is_skipped:
-        os.remove(file)
-        os.rename(dummy_file, file)
-    else:
-        os.remove(dummy_file)
+def check_history(response, __verbose):
+    try:
+        redirect_url = response.headers['Location']
+        return (True, redirect_url)
+    except KeyError:
+        __verbose and print("No redirection performed")
+        return (False, "")
+
 
 def test_https(link, __verbose):
     """
@@ -160,3 +151,27 @@ def get_all_status(file, links, __verbose, __exit):
                 )
             )
     return link_data
+
+def remove_down_links(file, line_numbers):
+    """In a file, delete the lines at line number in given list"""
+    is_skipped = False
+    counter = 0
+    # Create name of dummy / temporary file
+    dummy_file = file + '.bak'
+    # Open original file in read only mode and dummy file in write mode
+    with open(file, 'r') as read_obj, open(dummy_file, 'w') as write_obj:
+        # Line by line copy data from original file to dummy file
+        for line in read_obj:
+            # If current line number exist in list then skip copying that line
+            if counter not in line_numbers:
+                write_obj.write(line)
+            else:
+                is_skipped = True
+            counter += 1
+ 
+    # If any line is skipped then rename dummy file as original file
+    if is_skipped:
+        os.remove(file)
+        os.rename(dummy_file, file)
+    else:
+        os.remove(dummy_file)
